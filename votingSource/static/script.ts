0c2c933ed8cad75093239cb903a20a1393970735
Austin Green <austinjgreen94@yahoo.com>
2023-12-07 12:38:07 -0500

    orient everything in grid tables better

--- votingSource/static/script.ts
+++ votingSource/static/script.ts
@@ -1,41 +1,19 @@
-type submissionData = {
-    data: submissionResult[]
-    problemIDs: string[]
-}
-
-type submissionResult = {
-    uuid: string
-    submissionTime: string
-    songTitle: string
-    artistNames: string
-    gameNames: string
-    comments: string
-    submitterEmail: string
-    isAlt: boolean | string
-    filename: string
-    lyrics: string
-    score: number
-    identity: identity
-}
-
-type voteData = voteResult[]
-
-type voteResult = {
-    uuid: string
-    submissionTime: string
-    submitterEmail: string
-    score: number
-    identity: identity
-}
-
-type identity = {
-    ip: string
-    caller: string
-    userAgent: string
+type resultData = {
+    results: {
+        place: number
+        songTitle: string
+        rating: string
+        operand: string
+        adjustment: number
+    }[]
+    deviants: {
+       voter: string
+       deviance: number
+    }[]
 }
 
 async function fetchVoteData() {
-    let jsonData: voteData;
+    let jsonData: resultData;
     try {
         const response = await fetch('/');
         jsonData = await response.json();
@@ -43,13 +21,13 @@ async function fetchVoteData() {
         console.error('Error fetching data:', error);
     }
 
-    const mainTable = <HTMLTableElement>document.getElementById('mainTable');
-    displayData(mainTable, jsonData);
+    const mainTable = <HTMLTableElement>document.getElementById('leftTable');
+    const leftTextArea = <HTMLTextAreaElement>document.getElementById('leftTextArea')
+    displayData(leftTextArea, jsonData);
 }
 
 const fieldNameList : string[] = [];
 const editableFieldNames: string[] = ["songTitle", "artistNames", "gameNames", "comments", "isAlt", "lyrics"]
-const identityIndex: identity[] = [];
 
 function showModal(content) {
     let dialog = document.createElement("dialog");
@@ -71,74 +49,15 @@ function showModal(content) {
 }
 
 // Function to create and display table rows from JSON data
-function displayData(table :HTMLTableElement, data: submissionResult[]|voteResult[]) {
-    let head = table.createTHead()
+function displayData(textArea :HTMLTextAreaElement, data: resultData) {
+    let s = "";
 
-    //get title row names
-    const row = head.insertRow();
-
-    Object.keys(data[0]).forEach(key => {
-        if (key === "identity") {
-            return
-        }
-        row.insertCell().textContent = key;
-        fieldNameList.push(key);
+    data.results.forEach((item, index) => {
+        s = s.concat(`#${index+1} Artist - ${item.songTitle} - ${item.rating} ${item.operand}${item.adjustment}\n`)
     });
 
-    let body = table.createTBody()
-
-    data.forEach((item, index) => {
-
-        const saveButton = document.createElement('button');
-        saveButton.textContent = 'Save';
-        saveButton.addEventListener('click', () => {
-            saveButton.textContent = 'Save ⏳';
-            saveData(table, index + 1).then(r  =>{
-                saveButton.textContent = 'Save ✔';
-                removeChangedClassFromRow(table, index)
-            }, r => {
-                showModal(r)
-                console.error(r)
-                saveButton.textContent = 'Save ❌';
-            });
-        });
-
-        const row = body.insertRow();
-        Object.keys(item).forEach(key => {
-            const value = item[key]
-            if (key === "identity") {
-                identityIndex[index] = value;
-                return
-            }
-
-            const cell = row.insertCell();
-            let input: HTMLInputElement | HTMLTextAreaElement = document.createElement("input");
-            input.type = "text";
-            if (key === "comments" || key === "lyrics") {
-                input = document.createElement("textarea");
-                input.setAttribute("rows", "1");
-            }
-            if (key === "votes") {
-                input = document.createElement("textarea");
-                input.setAttribute("rows", "35");
-                input.setAttribute("cols", "100");
-                input.setAttribute("wrap", "off");
-            }
-            input.value = value;
-            if (!editableFieldNames.includes(key)) {
-                input.setAttribute("disabled", "true")
-            }
-            cell.insertAdjacentElement('afterbegin', input)
-
-            cell.addEventListener('input', () => {
-                input.classList.add("changed")
-                saveButton.classList.add("changed")
-            })
-
-        });
-
-        row.insertCell().appendChild(saveButton);
-    });
+    textArea.rows = data.results.length + 5;
+    textArea.textContent = s;
 }
 
 function removeChangedClassFromRow(table: HTMLTableElement, rowIndex: number) {
@@ -167,36 +86,34 @@ function removeChangedClassFromRow(table: HTMLTableElement, rowIndex: number) {
 }
 
 // Function to save edited data to the server
-async function saveData(table: HTMLTableElement, rowIndex: number) {
-    const cells = table.rows[rowIndex].cells;
-    const dataToSave: submissionResult = <submissionResult>{};
-
-    for (let i = 0; i < cells.length - 1; i++) {
-        const input = <HTMLInputElement|HTMLTextAreaElement>cells[i].querySelector('input,textarea');
-        const fieldName = fieldNameList[i];
-        dataToSave[fieldName] = input.value;
-        if (fieldName === "score") {
-            dataToSave[fieldName] = Number.parseFloat(input.value)
-        }
-    }
-
-    dataToSave.identity = identityIndex[rowIndex-1]
-
-    try {
-        const response = await fetch('/save', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(dataToSave)
-        });
-
-        if (response.ok) {
-            console.log('Data saved successfully!');
-        } else {
-            throw new Error('Failed to save data: ' + response.statusText);
-        }
-    } catch (error) {
-        throw new Error('Error saving data: ' +  error);
-    }
+async function postVoteSubmissionsToGetResults(table: HTMLTableElement) {
+    // const cells = table.rows[rowIndex].cells;
+    // const dataToSave: submissionResult = <submissionResult>{};
+    //
+    // for (let i = 0; i < cells.length - 1; i++) {
+    //     const input = <HTMLInputElement|HTMLTextAreaElement>cells[i].querySelector('input,textarea');
+    //     const fieldName = fieldNameList[i];
+    //     dataToSave[fieldName] = input.value;
+    //     if (fieldName === "score") {
+    //         dataToSave[fieldName] = Number.parseFloat(input.value)
+    //     }
+    // }
+    //
+    // try {
+    //     const response = await fetch('/', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(dataToSave)
+    //     });
+    //
+    //     if (response.ok) {
+    //         console.log('Data saved successfully!');
+    //     } else {
+    //         throw new Error('Failed to save data: ' + response.statusText);
+    //     }
+    // } catch (error) {
+    //     throw new Error('Error saving data: ' +  error);
+    // }
 }
